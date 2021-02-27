115d88d239a5e01c96226e4e5d2c35ba1b636e39
Popescu-George-Calin <calin.popescu@vtex.com.br>
2021-02-27 12:11:50 +0200

    private routing

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import PrivateRoute from './PrivateRoute';
+
 import {
   Root, Login, Form, Logout, Movies,
 } from '../pages';
@@ -9,7 +11,7 @@ export default () => (
     <Switch>
       <Route path="/" exact component={Root} />
       <Route path="/login" exact component={Login} />
-      <Route path="/form" exact component={Form} />
+      <PrivateRoute path="/form" exact component={Form} />
       <Route path="/logout" exact component={Logout} />
       <Route path="/movies" exact component={Movies} />
     </Switch>
